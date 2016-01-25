b4027e0d9fbaf21e5cfa5b462f9e51a174e78b2b
Garrett Hartley <ljgarretth@gmail.com>
2016-01-25 21:03:19 +0000

    trying to update

--- webServer/stat.cpp
+++ webServer/stat.cpp
@@ -1,8 +1,10 @@
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <unistd.h>
-#include "file.h"
 #include <iostream>
+#include <stdio.h>
+#include <dirent.h>
+#include <stdlib.h>
 
 using namespace std;
 
@@ -10,28 +12,31 @@ main(int argc, char **argv)
 {
 	struct stat filestat;
 
+
 	if(stat(argv[1], &filestat)) {
 		cout <<"ERROR in stat\n";
 	}
 	if(S_ISREG(filestat.st_mode)) {
 		cout << argv[1] << " is a regular file \n";
 		cout << "file size = "<<filestat.st_size <<"\n";
-		FILE fp = fopen(argv[1]);
-		char *buff = (char *)malloc(filestat.st_size);
+		FILE *fp = fopen(argv[1],"r");
+		char *buff = (char *)malloc(filestat.st_size+1);
 		fread(buff,filestat.st_size, 1, fp);
 		cout<<"FILE"<<endl<<buff<<endl;
+		free(buff);
+		fclose(fp);
 	}
 	if(S_ISDIR(filestat.st_mode)) {
 		cout << argv[1] << " is a directory \n";
-	  DIR *dirp;
-  		struct dirent *dp;
+		DIR *dirp;
+		struct dirent *dp;
 
-  	dirp = opendir(argv[1]);
-  	while ((dp = readdir(dirp)) != NULL)
-    	cout<<"name"<< dp->d_name<<endl;
-  	(void)closedir(dirp);	
-}
+		dirp = opendir(argv[1]);
+		while ((dp = readdir(dirp)) != NULL)
+			cout<<"name"<< dp->d_name<<endl;
+		(void)closedir(dirp);	
+	}
 
-		
+
 }
 
