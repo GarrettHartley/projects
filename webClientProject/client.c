e7776ff432a32aa2521e5b82c82320559212fe5c
garretthartley <ljgarretth@gmail.com>
2016-02-05 18:56:42 +0000

    working friday

--- webClientProject/client.c
+++ webClientProject/client.c
@@ -6,14 +6,16 @@
 #include <unistd.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <sys/epoll.h>
 
 #define SOCKET_ERROR        -1
 #define BUFFER_SIZE         100
 #define HOST_NAME_SIZE      255
+#define NCONNECTIONS 20
 
 int  main(int argc, char* argv[])
 {
-    int hSocket;                 /* handle to socket */
+    int hSocket[NCONNECTIONS];                 /* handle to socket */
     struct hostent* pHostInfo;   /* holds info about a machine */
     struct sockaddr_in Address;  /* Internet socket address stuct */
     long nHostAddress;
@@ -32,12 +34,15 @@ int  main(int argc, char* argv[])
         strcpy(strHostName,argv[1]);
         nHostPort=atoi(argv[2]);
       }
+	
 
     printf("\nMaking a socket");
     /* make a socket */
-    hSocket=socket(AF_INET,SOCK_STREAM,IPPROTO_TCP);
+int epollfd=epoll_create(1);
+for(int i = 0; i<NCONNECTIONS; i++){
+    hSocket[i] =socket(AF_INET,SOCK_STREAM,IPPROTO_TCP);
 
-    if(hSocket == SOCKET_ERROR)
+    if(hSocket[i] == SOCKET_ERROR)
     {
         printf("\nCould not make a socket\n");
         return 0;
@@ -56,22 +61,33 @@ int  main(int argc, char* argv[])
 /*    printf("\nConnecting to %s (%X) on port %d",strHostName,nHostAddress,nHostPort);
 
     /* connect to host */
-    if(connect(hSocket,(struct sockaddr*)&Address,sizeof(Address)) 
+    if(connect(hSocket[i],(struct sockaddr*)&Address,sizeof(Address)) 
        == SOCKET_ERROR)
     {
         printf("\nCould not connect to host\n");
         return 0;
     }
-
+	struct epoll_event event;
+	event.data.fd=hSocket[i];
+	event.events = EPOLLIN;
+	int ret = epoll_ctl(epollfd, EPOLL_CTL_ADD, hSocket[i],&event);
+	char request[]="GET /FOO.html HTTP/1.1\r\n\r\n";
     /* read from socket into buffer
     ** number returned by read() and write() is the number of bytes
     ** read or written, with -1 being that an error occured */
-    nReadAmount=read(hSocket,pBuffer,BUFFER_SIZE);
+    //nReadAmount=read(hSocket,pBuffer,BUFFER_SIZE);
     printf("\nReceived \"%s\" from server\n",pBuffer);
     /* write what we received back to the server */
-    write(hSocket,pBuffer,nReadAmount);
+    //write(hSocket[i],pBuffer,nReadAmount);
+	write(hSocket[i],pBuffer,strlen(request));
+}
+for(int i = 0; i < NCONNECTIONS; i++ ){
     printf("\nWriting \"%s\" to server",pBuffer);
-
+	struct epoll_event event;
+	int nr_events = epoll_wait(epollfd,&event, 1,-1);
+	char buffer[10000];
+	int rval = read(event.data.fd,buffer,10000);
+	printf("GOD %d from %d\nClosing socket\n",rval,event.data.fd);
     printf("\nClosing socket\n");
     /* close socket */                       
     if(close(hSocket) == SOCKET_ERROR)
@@ -79,4 +95,5 @@ int  main(int argc, char* argv[])
         printf("\nCould not close socket\n");
         return 0;
     }
+}
 }
